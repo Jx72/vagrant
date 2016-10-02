e658a66b0731041d0e200fdcd8b3f09a3901779b
Jerry Xu <xujianrui@gmail.com>
2016-10-02 22:16:37 +1100

    fix some bugs

--- centos-init.sh
+++ centos-init.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 main() {
-	echo $HOSTNAME is initailizing the system
+	echo "$HOSTNAME is initailizing the system"
 	add_repo
 	update
+	echo "centos-init is completed!"
 }
 
 update() {
-	echo Update the CentOS 7 system...
+	echo "Update the CentOS 7 system..."
 	yum update -y >> /tmp/centos-init.log 2>&1
 }
 
 add_repo() {
-	echo Add the EPEL repository
+	echo "Add the EPEL repository"
 	yum install -y epel-release >> /tmp/centos-init.log 2>&1
 }
 
--- centos-lamp.sh
+++ centos-lamp.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 main() {
-	echo Install Linux, Apache, MySQL, PHP (LAMP) stack On CentOS 7
+	echo "Install Linux, Apache, MySQL, PHP (LAMP) stack On CentOS 7"
 	install_apache
 	install_mysql
 	install_php
-	echo Processing of LAMP is completed!
+	echo "Processing of LAMP is completed!"
 }
 
 install_apache() {
-	echo Install Apache and start the web service
+	echo "Install Apache and start the web service"
 	yum install -y httpd >> /tmp/centos-lamp.log 2>&1
 	systemctl start httpd.service >> /tmp/centos-lamp.log 2>&1
 	systemctl enable httpd.service >> /tmp/centos-lamp.log 2>&1
 }
 
 install_mysql() {
-	echo Install MySQL (MariaDB) and start the database service
+	echo "Install MySQL (MariaDB) and start the database service"
 	yum install -y mariadb-server mariadb >> /tmp/centos-lamp.log 2>&1
 	systemctl start mariadb.service >> /tmp/centos-lamp.log 2>&1
 	systemctl enable mariadb.service >> /tmp/centos-lamp.log 2>&1
@@ -33,7 +33,7 @@ EOF >> /tmp/centos-lamp.log 2>&1
 }
 
 install_php() {
-	echo Install PHP
+	echo "Install PHP"
 	yum install -y php php-pear php-mysql phpMyAdmin >> /tmp/centos-lamp.log 2>&1
 	systemctl restart httpd.service >> /tmp/centos-lamp.log 2>&1
 }
