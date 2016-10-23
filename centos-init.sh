f4b051438afec1ad2d3852f64cea0ecfbe765b71
Jerry Xu <xujianrui@gmail.com>
2016-10-23 15:45:37 +1100

    fix bugs

--- centos-init.sh
+++ centos-init.sh
@@ -7,14 +7,19 @@ main() {
 	echo "centos-init is completed!"
 }
 
+add_repo() {
+	echo "Add the EPEL repository"
+	yum install -y epel-release >& /tmp/centos-init.log
+}
+
 update() {
 	echo "Update the CentOS 7 system..."
 	yum update -y >& /tmp/centos-init.log
 }
 
-add_repo() {
-	echo "Add the EPEL repository"
-	yum install -y epel-release >& /tmp/centos-init.log
+install_com_sw() {
+	echo "Install some common utilities"
+	yum install -y tree
 }
 
 main
